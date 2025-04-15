4affdfb9c6e0e3226640dc0a385efc34ccbc3c73
ttiopp3 <noorgmll338@gmail.com>
2025-04-15 22:58:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 
 
-API_TOKEN = '5730303832:AAGE3lbjVqNaqJ0bUww-eRUzW_dNTkrJXrg'
+API_TOKEN = '8151839888:AAFSKa2K6Ns8wAG8wLhJY1JbUIFmz1ylkgk'
 DB_URI ="postgresql://postgres:SEvju9ySxpC7lfoeXwwU@containers-us-west-79.railway.app:7370/railway"
 
 bot = Bot(token=API_TOKEN)
